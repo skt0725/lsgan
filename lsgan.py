959b0a514a12f9fb5c26d28f39081b7a4068f4d3
skt0725 <skt0725@kaist.ac.kr>
2022-07-13 21:32:24 +0900

    lsgan - a : 0, b : 1, c :1

--- lsgan.py
+++ lsgan.py
@@ -15,16 +15,18 @@ os.environ["CUDA_VISIBLE_DEVICES"]= "0, 1"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 transforms = transforms.Compose([
+    transforms.Resize(64),
     transforms.ToTensor(),
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
+# resize to 64 (32 too small)
 
 # hyperparameter setting
 data_dir = "./data"
-batch_size = 128
-latent_size = 64
-total_epoch = 200
-learning_rate = 0.0001
+batch_size = 64
+latent_size = 100
+total_epoch = 50
+learning_rate = 0.0002
 train_data = datasets.CIFAR10(root = data_dir, train = True, transform = transforms, download = True)
 dataloader = DataLoader(dataset = train_data, batch_size = batch_size, shuffle = True, num_workers = 4)
 
@@ -32,30 +34,35 @@ images, labels = next(iter(dataloader))
 img = images[0].squeeze()
 image = torch.permute(images[0].squeeze(), (1,2,0))
 label = labels[0]
-plt.imshow(image)
-plt.title(label = label)
-plt.show()
+# plt.imshow(image)
+# plt.title(label = label)
+# plt.show()
 
 class Generator(nn.Module):
     def __init__(self, latent_size):
         super().__init__()
         self.conv1= nn.Sequential(
-            nn.ConvTranspose2d(64, 512, 4, 1),
+            nn.ConvTranspose2d(latent_size, 512, 4, 1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(inplace = True)
+            nn.ReLU(inplace = True)
         )
         self.conv2 = nn.Sequential(
             nn.ConvTranspose2d(512, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(inplace = True)
+            nn.ReLU(inplace = True)
         )
         self.conv3 = nn.Sequential(
             nn.ConvTranspose2d(256, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace = True)
+            nn.ReLU(inplace = True)
         )
         self.conv4 = nn.Sequential(
-            nn.ConvTranspose2d(128, 3, 4, 2, 1),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.ConvTranspose2d(64, 3, 4, 2, 1),
             nn.Tanh()
         )
 
@@ -64,44 +71,41 @@ class Generator(nn.Module):
         output = self.conv2(output)
         output = self.conv3(output)
         output = self.conv4(output)
+        output = self.conv5(output)
         return output
 
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 2, 1),
-            nn.ReLU(inplace = True)
+            nn.Conv2d(3, 64, 4, 2, 1),
+            nn.LeakyReLU(inplace = True)
         )
         self.conv2= nn.Sequential(
-            nn.Conv2d(64, 128, 3, 1),
+            nn.Conv2d(64, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
-            nn.ReLU(inplace = True)
+            nn.LeakyReLU(inplace = True)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, 1),
+            nn.Conv2d(128, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
-            nn.ReLU(inplace = True)
+            nn.LeakyReLU(inplace = True)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 512, 3, 1),
+            nn.Conv2d(256, 512, 4, 2, 1),
             nn.BatchNorm2d(512),
-            nn.ReLU(inplace = True)
+            nn.LeakyReLU(inplace = True)
         )
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(51200, 1),
-            nn.Sigmoid()
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512, 1, 4, 1, 0)
         )
     def forward(self, z):
-        # print(z.size())
         output = self.conv1(z)
         output = self.conv2(output)
         output = self.conv3(output)
         output = self.conv4(output)
-        # print(output.size())
-        output = self.fc(output)
-        return output
+        output = self.conv5(output)
+        return output.view(-1, 1)
 
 discriminator = Discriminator().to(device)
 generator = Generator(latent_size).to(device)
@@ -123,7 +127,6 @@ for epoch in range(total_epoch):
         z = torch.randn((image.size(0), latent_size, 1, 1)).to(device)
         fake_image = generator(z)
         fake_output = discriminator(fake_image.detach())
-
         real_loss = criterion(real_output, b)/2
         fake_loss = criterion(fake_output, a)/2
         discriminator_loss = real_loss + fake_loss
@@ -143,10 +146,10 @@ for epoch in range(total_epoch):
     if not os.path.exists(dir):
         os.makedirs(dir)
     if epoch == 0:
-        real_image = real_image.view(real_image.size(0), 3, 32, 32)
+        real_image = real_image.view(real_image.size(0), 3, 64, 64)
         save_image(real_image, "./result/real.png", normalize=True)
     if epoch % 10 == 0:
-        fake_image = fake_image.view(fake_image.size(0), 3, 32, 32)
+        fake_image = fake_image.view(fake_image.size(0), 3, 64, 64)
         # dir = f"./result/detach"
         save_image(fake_image, os.path.join(dir, f"{epoch}.png"), normalize=True)
     t = time()-start_time
